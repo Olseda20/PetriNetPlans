44193cb676c9816b8b0c68cf45479038de1045bb
Kyro <49950899+Olseda20@users.noreply.github.com>
2024-04-03 14:13:04 +0100

    moving debug check into pnp_cmd_base to allow continuous updating of the debug file

--- PNPros/ROS_bridge/pnp_ros/scripts/pnp_cmd_base.py
+++ PNPros/ROS_bridge/pnp_ros/scripts/pnp_cmd_base.py
@@ -8,68 +8,67 @@ import sys
 import os
 import time
 
-class tcol:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
 
+class tcol:
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+
+debug_actions_path = "debug_config.yaml"
 
 class PNPCmd_Base(object):
 
     def __init__(self):
-        self.execlevel = 0 # pretty print
+        self.execlevel = 0  # pretty print
 
         # to store Execution Rules
         self._action_ers = {}
 
-
     def action_cmd_base(self, action, params, cmd):
         self.printindent()
-        if (cmd=='start'):
-            print("  %s%s %s -> %s%s" %(tcol.OKGREEN,action,params,cmd,tcol.ENDC))
-        elif (cmd=='interrupt'):
-            print("  %s%s %s -> %s%s" %(tcol.WARNING,action,params,cmd,tcol.ENDC))
-        elif (cmd=='end' or cmd=='success'):
-            print("  %s%s %s -> %s%s" %(tcol.OKGREEN,action,params,cmd,tcol.ENDC))
-        elif (cmd=='failure'):
-            print("  %s%s %s -> %s%s" %(tcol.FAIL,action,params,cmd,tcol.ENDC))
+        if cmd == "start":
+            print("  %s%s %s -> %s%s" % (tcol.OKGREEN, action, params, cmd, tcol.ENDC))
+        elif cmd == "interrupt":
+            print("  %s%s %s -> %s%s" % (tcol.WARNING, action, params, cmd, tcol.ENDC))
+        elif cmd == "end" or cmd == "success":
+            print("  %s%s %s -> %s%s" % (tcol.OKGREEN, action, params, cmd, tcol.ENDC))
+        elif cmd == "failure":
+            print("  %s%s %s -> %s%s" % (tcol.FAIL, action, params, cmd, tcol.ENDC))
         else:
-            print("  %s %s -> %s" %(action,params,cmd))
-        self.action_cmd(action,params,cmd)
-
+            print("  %s %s -> %s" % (action, params, cmd))
+        self.action_cmd(action, params, cmd)
 
     def action_params_split(self, astr):
         astr = astr.strip()
-        c = astr.find('_')
+        c = astr.find("_")
         if c < 0:
             action = astr
-            params = ''
+            params = ""
         else:
             action = astr[0:c]
-            params = astr[c+1:]
+            params = astr[c + 1 :]
         return [action, params]
 
-
     def execRecovery(self, recovery):
-        if recovery=='':
+        if recovery == "":
             return
-        v = recovery.split(';')
+        v = recovery.split(";")
         for i in range(len(v)):
             ap = v[i].strip()
             self.printindent()
-            print('%s-- recovery %s%s' %(tcol.WARNING,ap,tcol.ENDC))
-            if (ap=='restart_action'):
+            print("%s-- recovery %s%s" % (tcol.WARNING, ap, tcol.ENDC))
+            if ap == "restart_action":
                 return ap
-            elif (ap=='skip_action'):
+            elif ap == "skip_action":
                 return ap
-            elif (ap=='restart_plan'):
+            elif ap == "restart_plan":
                 return ap
-            elif (ap=='fail_plan'):
+            elif ap == "fail_plan":
                 return ap
             else:
                 [action, params] = self.action_params_split(ap)
@@ -77,74 +76,154 @@ class PNPCmd_Base(object):
 
     def printindent(self):
         for i in range(self.execlevel):
-            print('    ', end="")
+            print("    ", end="")
 
-    def exec_action(self, action, params, interrupt='', recovery=''):
+    def is_debug_action(self, action):
+        debug_actions = self.get_debug_actions(debug_actions_path)
+        if debug_actions["Debug"] is False:
+            return False
+
+        mode = self.debug_actions["value"]["mode"]
+        msg = self.debug_actions["msg"]
+        type = self.debug_actions["value"]["type"]
+        actions = self.debug_actions["value"]["actions"]
+
+        if type == "enable":
+            if action in actions:
+                return {'type': type, 'mode':mode, 'perform_action' = True}
+            return {'type': type, 'mode': mode, 'perform_action' = False}
+
+        if type == "disable":
+            if action in actions:
+                return {'type': type, 'mode': mode, 'perform_action' = False}
+            return {'type': type, 'mode':mode, 'perform_action' = True}
+
+
+    def get_debug_actions(debug_config_path):
+        if not os.path.exists(debug_config_path):
+            return {"Debug": False, "msg": "Debug file was not found.", "value": {}}
+
+        with open(debug_config_path) as f:
+            debug_actions = yaml.safe_load(f)
+
+        if debug_actions["Debug"]["Active"] is False:
+            return {"Debug": False, "msg": "Debug mode is not active", "value": {}}
+
+        mode = debug_actions["Debug"]["Mode"]
+        config = debug_actions["Configurations"]
+
+        if mode not in config.keys():
+            return {
+                "Debug": False,
+                "msg": "Debug mode is not found in configurations",
+                "value": {},
+            }
+
+        if "Active" in config[mode].keys() and config[mode]["Active"] is not None:
+            return {
+                "Debug": True,
+                "msg": "Debug mode active",
+                "value": {
+                    "mode": mode,
+                    "type": "enable",
+                    "actions": config[mode]["Active"],
+                },
+            }
+
+        if "Inactive" in config[mode].keys() and config[mode]["Inactive"] is not None:
+            return {
+                "Debug": True,
+                "msg": "Debug mode active",
+                "value": {
+                    "mode": mode,
+                    "type": "disable",
+                    "actions": config[mode]["Inactive"],
+                },
+            }
+
+        # if no actions are found
+        return {"Debug": False, "msg": "Debug mode did not find any actions.", "value": {}}
+
+    def exec_action(self, action, params, interrupt="", recovery=""):
         self.printindent()
-        print("%sExec: %s %s %s" %(tcol.OKGREEN,action,params,tcol.ENDC))
+        print("%sExec: %s %s %s" % (tcol.OKGREEN, action, params, tcol.ENDC))
+
+        # TODO: Check if this action is part of the debug modes
+        if action is not '':
+            print("DEBUG MODE: %s" % debug)
+            if debug not in self.debug_actions:
+                return "success"
+
+        self.debug_actions:
+        #     self.debug_action(action, params)
+        #     return r
 
         run = True
 
         # add the ER
-        if interrupt != '' and recovery != '':
+        if interrupt != "" and recovery != "":
             self.add_ER(action, interrupt, recovery)
 
-        while (run): # interrupt may restart this action
+        while run:  # interrupt may restart this action
 
-            self.action_cmd_base(action, params, 'start')
+            self.action_cmd_base(action, params, "start")
 
             # the PNP action server will change the status to running after it stated
             # the action. Therefore we wait here for that to happen.
 
-            while (self.action_status(action) == "started"):
+            while self.action_status(action) == "started":
                 time.sleep(0.1)
 
             # wait for action to terminate
-            r = 'running'
+            r = "running"
             c = False
-            while (r=='running' and not c):
+            while r == "running" and not c:
                 r = self.action_status(action)
                 # check for interrupt condition
                 c, rec = self._check_interrupt_conditions(action)
 
                 time.sleep(0.1)
-            print("   -- action status: %s, interrupt condition: %r" %(r,c))
+            print("   -- action status: %s, interrupt condition: %r" % (r, c))
             run = False  # exit
-            if (not c): # normal termination
+            if not c:  # normal termination
                 self.printindent()
                 self.action_cmd_base(action, params, r)
                 # if (r=='success'):
                 #     print("  %s%s %s -> %s%s" %(tcol.OKGREEN,action,params,r,tcol.ENDC))
                 # else:
                 #     print("  %s%s %s -> %s%s" %(tcol.FAIL,action,params,r,tcol.ENDC))
-            else: # interrupt
-                r = 'interrupt'
+            else:  # interrupt
+                r = "interrupt"
                 self.action_cmd_base(action, params, r)
-                while self.action_status(action)=='running':
+                while self.action_status(action) == "running":
                     time.sleep(0.1)
                 self.execlevel += 1
                 p_rec = self.execRecovery(rec)
                 self.execlevel -= 1
-                if p_rec=='restart_action':
+                if p_rec == "restart_action":
                     run = True
-                elif p_rec=='fail_plan':
+                elif p_rec == "fail_plan":
                     self.end()
                 else:
-                    print("[ERROR] The recovery procedure "+ p_rec + " may not be implemented. I am just stopping the current action and continuing with the plan.")
+                    print(
+                        "[ERROR] The recovery procedure "
+                        + p_rec
+                        + " may not be implemented. I am just stopping the current action and continuing with the plan."
+                    )
         return r
 
     def _check_interrupt_conditions(self, action):
         c = False
-        rec = ''
+        rec = ""
 
         if action in self._action_ers.keys():
             # print self._action_ers[action].items()
             for interrupt, recovery in self._action_ers[action].items():
-                if (interrupt[0:7].lower()=='timeout'):
+                if interrupt[0:7].lower() == "timeout":
                     now = time.time()
                     st = self.action_starttime(action)
-                    v = interrupt.split('_')
-                    #print 'timeout %f %f diff: %f > %f' %(now, st, now-st, float(v[1]) )
+                    v = interrupt.split("_")
+                    # print 'timeout %f %f diff: %f > %f' %(now, st, now-st, float(v[1]) )
                     c = (now - st) > float(v[1])
                 else:
                     c = self.get_condition(interrupt)
@@ -154,7 +233,7 @@ class PNPCmd_Base(object):
                     return c, rec
 
         # else:
-            # print "No ers for action ", action
+        # print "No ers for action ", action
         return c, rec
 
     def add_ER(self, action, interrupt, recovery):
@@ -166,9 +245,22 @@ class PNPCmd_Base(object):
         self._action_ers[action][interrupt] = recovery
 
     def plan_gen(self, planname):
-        oscmd = 'cd %s; ./genplan.sh %s.plan %s.er' % (self.plan_folder, planname, planname)
+        oscmd = "cd %s; ./genplan.sh %s.plan %s.er" % (
+            self.plan_folder,
+            planname,
+            planname,
+        )
         print(oscmd)
         os.system(oscmd)
+
+    def debug_mode_evaluation(self, debug):
+        '''Read the debug yaml file and determine if the action should run or not'''
+        # read debug_config.yaml
+        with open('debug_config.yaml', 'r') as file:
+            debug_config = yaml.load(file, Loader=yaml.FullLoader)
+        if debug_config in debug_config:
+
+        if debug is not in Mode
 
     # TO BE IMPLEMENTED BY SUBCLASSES
 
@@ -178,14 +270,14 @@ class PNPCmd_Base(object):
     def end(self):
         pass
 
-    def action_cmd(self,action,params,cmd): # non-blocking
+    def action_cmd(self, action, params, cmd):  # non-blocking
         pass
 
     def set_action_status(self, action, status):
-        return ''
+        return ""
 
     def action_status(self, action):
-        return ''
+        return ""
 
     def get_condition(self, cond):
         return False
@@ -193,11 +285,12 @@ class PNPCmd_Base(object):
     def set_condition(self, cond, value):
         return
 
-    def plan_cmd(self, planname, cmd): # non-blocking
+    def plan_cmd(self, planname, cmd):  # non-blocking
         pass
 
     def plan_name(self):
-        return 'no plan'
+        return "no plan"
 
     def plan_status(self):
-        return 'no status'
+        return "no status"
+
